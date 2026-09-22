0cbdf604c68e222c4551ce0bc6465508ab677994
Fatih Kenan <fkaya253412@gmail.com>
2026-09-22 12:46:31 +0300

    dbcontext oluşturuldu

---
+++ Persistence/Contexts/WalletDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    public class WalletDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Integrated Security = True;mssqllocaldb;Database=WalletDb;TrustServerCertificate = True;");
+
+        }
+    }
+}
